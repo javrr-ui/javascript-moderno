4efab60bebf47ca25a19d5bb1c8c7ed139f653c2
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-11 00:18:29 -0800

    concatenar strings

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,12 @@
+//concatenar un string y template strings
+const producto = "monitor de 20 pulgadas ";
+const precio = "30 dolares ";
+
+//console.log(producto.concat(precio));
+
+
+console.log(producto + "tiene un precio de: "+precio);
+console.log(producto , "tiene un precio de: ",precio);
+
+console.log(`El producto ${producto} tiene un precio de ${precio}`);
+
