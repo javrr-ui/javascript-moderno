dc8148e399d28dd87d1c15c3ad0f1b54480781cc
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-10 23:47:35 -0800

    string en javascript

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,8 @@
+const producto = "monitor";
+console.log(producto);
+
+const producto2 = String("monitor de 24 pulgadas");
+console.log(producto2);
+
+const producto3 = new String("monitor de 27 pulgadas");
+console.log(producto3);
