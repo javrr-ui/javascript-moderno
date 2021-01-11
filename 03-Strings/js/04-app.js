8fe60196595187b52a8776898812c09378804326
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-11 00:51:12 -0800

    cortar espacios en string

---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,6 @@
+//cortar espacios en blanco en un string
+
+const producto = "                         monitor de 24 pulgadas        ";
+//console.log(producto)
+console.log(producto.trimStart());
+console.log(producto.trimEnd());
