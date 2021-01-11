a9549cea915207a33a04888bd58438f615911dbb
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-11 00:08:07 -0800

    metodos de strings

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,10 @@
+//string methods
+
+const producto5 = "celular";
+console.log(producto5);
+
+//conocer la cantidad de letras de la cadena de texto
+console.log(producto5.length);
+
+//conocer si un string contiene una palabra 
+console.log(producto5.includes("monitor"));
