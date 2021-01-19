ec74e33bcbd37f4bb06e4f67acaf883f0bd57475
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-18 23:24:21 -0800

    Operacines numericas en javascript

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,28 @@
+//operaciones en javascript
+
+const numero1 = 10;
+const numero2 = 20;
+
+let resultado;
+
+
+//suma 
+resultado = numero1 + numero2;
+console.log(resultado);
+
+//resta
+resultado = numero1 - numero2;
+console.log(resultado);
+
+//multiplicacion
+resultado = numero1 * numero2;
+console.log(resultado);
+
+//division
+
+resultado = numero1 / numero2;
+console.log(resultado);
+
+//modulo
+resultado = numero1 % numero2;
+console.log (resultado);
