75157b5ea9bfcda4ee4d493c48b6609c28eeb91b
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-18 23:15:47 -0800

    Números en javascript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,14 @@
+// numeros en javascript
+
+
+const numero1 = 10;
+const numero2 = "10";
+const numero3 = .234234;
+const numero4 = -239;
+
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
