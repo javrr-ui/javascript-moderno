3bac686b842d2d663541789cb08066cb609b93a4
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-20 15:56:04 -0800

    Operador mayor que y menor que

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,12 @@
+const num1 = 20;
+const num2 = "20";
+const num3 = 30;
+
+//operador mayor que
+
+
+console.log(num1 > num3);
+console.log(num3 > num1);
+
+// Operador menor que
+console.log(num1 < num3);
