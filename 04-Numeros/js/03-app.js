f7e1b2edf61f98381ca869181da67972bf7cd2ce
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-19 21:45:18 -0800

    Objeto Math en javascript

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,35 @@
+//El objeto math
+
+
+let resultado;
+
+
+// PI
+resultado = Math.PI;
+// Redondear
+resultado = Math.round(3.8);
+
+//redondear hacia arriba
+resultado = Math.ceil(2.7);
+
+//redondear hacia abajo
+resultado = Math.floor(2.9);
+
+//raiz cuadrada
+resultado = Math.sqrt(64);
+
+
+//valor absoluto
+resultado = Math.abs(-200);
+
+//potencia
+resultado = Math.pow(2, 4);
+
+//numero minimo
+resultado = Math.min(3,2, 8, 15, 0);
+
+//numero maximo
+resultado = Math.max(3,2, 8, 15, 0);
+
+
+console.log(resultado);
