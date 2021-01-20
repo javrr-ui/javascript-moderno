ce1b3ca80249675f1a8cd62070e14d76fb3b2550
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-19 22:00:51 -0800

    orden de operaciones e incrementos y decrementos

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,12 @@
+//Orden de las operaciones
+
+let resultado;
+
+//primero son multiplicaciones y divisiones y luego sumas y restas
+
+resultado = 20 + 40 * 2; //100
+
+
+
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,14 @@
+//incrementos y decrementos
+
+let puntaje = 5;
+
+puntaje++; //En consola muestra el valor de puntaje y posteriormente añade uno
+
+++puntaje; //añade a puntaje y despues muestra su valor
+
+//Para agregar mas de un uno
+puntaje+= 3;
+
+//Para quitar mas de uno
+puntaje-= 3;
+
