5a64fefd5683a07607e024cac8369bb8f3a775cb
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-20 23:34:00 -0800

    Como acceder a los valores de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,11 @@
+//Como acceder a los valores de un objeto
+
+const producto = { 
+    nombre: "Monitor de 20 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto.precio);
+console.log(producto.nombre);
+console.log(producto.disponible);
