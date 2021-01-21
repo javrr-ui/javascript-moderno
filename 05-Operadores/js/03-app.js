87b6685612e6aca0dd6fe1a381fbec16b271cd6a
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-20 22:38:58 -0800

    comparar null y undefined

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,10 @@
+// Comparar null y undefined
+
+//Undefined
+let numero;
+console.log(numero); //Se muestra un "undefined" en la consola
+
+//Null
+let numero2 = null;
+console.log(numero2);
+console.log(typeof numero2);
