a147d462fc06b87f539e3786f7da408d4861ec06
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-22 17:30:00 -0800

    Acceder a los valores de un array

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,11 @@
+//Acceder a los valores de un array
+
+const numeros = [ 10,20,30, 40, 50];
+
+
+//console.table para mostrar una tabla en consola
+
+console.table(numeros);
+
+//Acceder al arreglo
+console.log(numeros[0]);
