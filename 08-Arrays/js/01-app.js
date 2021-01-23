70f6f94e835dc41aa7ab0f31b8b7830d55fdcd02
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-22 17:25:20 -0800

    Crear arrays en javascript

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,11 @@
+//Arrays en javascript
+
+const numeros = [10, 20, 30];
+
+
+const meses = new Array("Enero","Febrero","Marzo");
+
+
+//Un arrego con multiples elementos de diferentes tipos
+const deTodo = ['Hola',10, null, "No", {nombre: "Francisco", ocupacion: "Estudiante"},[1,2,3]];
+console.log(deTodo);
