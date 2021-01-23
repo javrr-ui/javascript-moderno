880945bdd0b1affb59c56c98047645ac2808b0fb
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-22 18:49:11 -0800

    Recorrer un arreglo

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,11 @@
+
+
+const meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio"];
+
+console.table(meses);
+
+console.log(`El array contiene ${meses.length} elementos`);
+
+for(let i=0; i < meses.length;i++){
+    console.log(meses[i]);
+}
