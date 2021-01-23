27890f1e7853c0b41d19b98212a7ea70393f4f37
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-22 18:55:09 -0800

    Agregar nuevos valores a un array

---
+++ 08-Arrays/js/04-app.js
@@ -0,0 +1,13 @@
+//Agregar nuevos valores a un array
+
+const meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio", "Julio"];
+
+//reescribir un valor
+meses[0] = "Nuevo valor";
+
+//agregar un nuevo valor al arreglo
+meses[7]="Agosto";
+// se salta los espacios en blanco
+meses[10] = "Ultimo mes";
+
+console.table(meses);
