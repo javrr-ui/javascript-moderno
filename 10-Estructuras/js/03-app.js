a9dc2100b54b9c6122b29e740e4c7e5406ad3a2d
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-23 19:58:17 -0800

    mayor que menor que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,10 @@
+//Comparando mayor que o menor que
+
+const dinero = 500;
+const totalAPagar = 300;
+
+if(dinero >= totalAPagar){
+    console.log("si podemos pagar");
+}else {
+    console.log("fondos insuficientes");
+}
