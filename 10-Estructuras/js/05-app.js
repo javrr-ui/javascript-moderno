a900238d39f1632d9bbbca818dc3254435156034
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-23 20:37:25 -0800

    Switch

---
+++ 10-Estructuras/js/05-app.js
@@ -0,0 +1,21 @@
+//Switch para condiciones multiples
+
+
+const metodoPago = "efectivos";
+
+switch(metodoPago){
+    case  "efectivo":
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case "cheque":
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case "tarjeta":
+         console.log(`Pagaste con ${metodoPago}`);
+         break;
+    
+    default:
+        console.log("Aun no has seleccionado un metodo de pago o metodo de pago no soportado");
+        break;
+
+}
