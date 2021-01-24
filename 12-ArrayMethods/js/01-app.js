f076cdb12b1dbc3e4f34b931b8ca1f15902e923b
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-24 10:01:51 -0800

    .some

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -8,3 +8,30 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+
+//comprobar si un valor existe en un arreglo de manera manual
+
+meses.forEach(mes => {
+    if(mes === 'Enero'){
+        console.log("si incluye enero");
+    }
+});
+
+
+//metodo includes
+//No funciona para arreglo de objetos, solo arreglos simples
+const resultado = meses.includes('Enero');
+console.log(resultado);
+
+//En un arreglo de objetos se usa .some
+
+
+const existe = carrito.some( producto => producto.nombre === 'Tablet');
+console.log(existe);
+
+
+//en un arreglo normal
+
+const existe2 = meses.some( mes => mes === 'Febrero');
+console.log(existe2);
