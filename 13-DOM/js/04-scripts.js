4a1cf782c0ba95b32b8cc12ed3d35121babbe425
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-24 15:24:51 -0800

    querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,26 @@
+// querySelector
+
+const card = document.querySelector('.card');
+console.log(card);
+
+//podemos tener selectores especificos como en css
+
+const info = document.querySelector(".premium .info");
+console.log(info);
+
+
+//Seleccionar el segundo card de hospedaje
+
+const segundoCard = document.querySelector(".hospedaje .card:nth-child(2)");
+console.log(segundoCard);
+
+
+//seleccionar el formulario
+
+const formulario = document.querySelector("#formulario");
+console.log(formulario);
+
+//seleccionar elemento por su etiqueta html
+
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
