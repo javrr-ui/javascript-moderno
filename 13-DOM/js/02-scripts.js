d66bf92c74675f756c1ab2e1b11f93a5859cd759
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-24 14:37:42 -0800

    getElementsByClassName

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,10 @@
+//getElementsByClassName
+
+const elemento = document.getElementsByClassName("header");
+console.log(elemento);
+
+const hero = document.getElementsByClassName('hero');
+console.log(hero);
+
+const contenedores = document.getElementsByClassName("contenedor");
+console.log(contenedores);
