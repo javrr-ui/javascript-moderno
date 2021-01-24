0ebf34086afbca49d0f764b2fb2ba16e27acd24f
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-23 20:57:54 -0800

    Operador &&

---
+++ 10-Estructuras/js/06-app.js
@@ -0,0 +1,10 @@
+// operador && 
+
+const usuario = true;
+const puedePagar = true;
+
+if(usuario&&puedePagar){
+    console.log("Si puedes comprar");
+}else{
+    console.log("No puedes pagar");
+}
