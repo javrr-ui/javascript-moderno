2c47d65f02899c3e4ddc41a425989000a5b81720
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-23 18:03:06 -0800

    Ventajas de los arrow functions

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,11 @@
+// Ventajas de los arrow functions
+
+const aprendiendo = function(){
+    console.log("Aprendiendo javascript");
+}
+
+//cuando recibe solo un parametro no es necesario pone los parentesis
+const aprendiendo2 = tecnologia => `Aprendiendo ${tecnologia}`;
+
+
+console.log(aprendiendo2("Javascript"));
