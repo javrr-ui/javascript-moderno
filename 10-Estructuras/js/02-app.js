9ddfee7197d4c347ba416d43677097948ee76d1d
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-23 19:25:44 -0800

    Comparador estricto

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,24 @@
+//Comparador estricto
+
+
+const puntaje = 1000;
+
+// != diferemte
+// == igual a
+if(puntaje == 1000){
+    console.log("si es igual");
+}else{
+    console.log("no es igual");
+}
+
+
+if(puntaje === "1000"){
+    console.log("si es igual");
+}else{
+    console.log("no es igual");
+}
+
+// == operador que no es estricto
+// == operador que si es estricto
+
+
