4f152422573b41ca942ad4bf5976a0d173d13ac4
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-24 19:23:30 -0800

    Ejemplo avanzado de javascript

---
+++ 13-DOM/js/11-scripts.js
@@ -0,0 +1,20 @@
+//Ejemplo avanzado
+
+
+const btnFlotante = document.querySelector(".btn-flotante");
+const footer = document.querySelector(".footer");
+
+
+btnFlotante.addEventListener("click",mostrarOcultarFooter);
+
+
+function mostrarOcultarFooter(){
+    if(footer.classList.contains("activo")){
+        footer.classList.remove("activo");
+        this.classList.remove("activo");
+    }else{
+        footer.classList.add("activo");
+        this.classList.add("activo");
+    }
+    
+}
