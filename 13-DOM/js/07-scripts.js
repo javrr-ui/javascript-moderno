607638748ee3d70c721f533e43a283c359c28bbe
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-24 16:15:25 -0800

    Cambiando el CSS con javascript

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,11 @@
+//Cambiando el CSS con javascript
+
+
+// const encabezado = document.querySelector("h1");
+// encabezado.style.backgroundColor = "black";
+
+
+//añadir una claas a un elemento en el Html
+
+const card = document.querySelector(".card");
+card.classList.add("nueva-clase","segunda-clase");  //.remove
