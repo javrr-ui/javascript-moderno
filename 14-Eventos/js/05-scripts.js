85b0b10c9c9d6b19d26d1235b81677842b646526
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-25 18:40:52 -0800

    Eventos al dar scroll con el mouse

---
+++ 14-Eventos/js/05-scripts.js
@@ -0,0 +1,14 @@
+//Eventos al dar scroll con el mouse
+
+window.addEventListener("scroll", () => {
+
+    const premium = document.querySelector(".premium");
+    const ubicacion = premium.getBoundingClientRect();
+
+    if(ubicacion.top < 784) {
+        console.log("El elemento ya está visible")
+    }{
+        console.log("Aun no, da mas scroll");
+    }
+
+});
