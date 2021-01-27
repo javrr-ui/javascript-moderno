cd3c9e47d46239f7db8569faebf8b4256b523a1a
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-26 17:57:24 -0800

    Primeros pasos

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,20 @@
+//variables
+
+
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+
+cargarEventListeners();
+function cargarEventListeners(){
+
+    listaCursos.addEventListener("click", agregarCurso);
+}
+
+function agregarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains("agregar-carrito")){
+        console.log("agregando al carrito");
+    }
+}
