f4e0dc282632bb0420a493b789f82e97276c8e4d
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-27 18:06:35 -0800

    Leer los datos del curso que seleccionamos

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -15,6 +15,23 @@ function cargarEventListeners(){
 function agregarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains("agregar-carrito")){
-        console.log("agregando al carrito");
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+// lee el contenido del html
+
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector("img").src,
+        titulo: curso.querySelector("h4").textContent,
+        precio: curso.querySelector(".precio span").textContent,
+        id: curso.querySelector("a").getAttribute("data-id"),
+        cantidad: 1,
+    }
+    console.log(infoCurso);
 }
