90e88d1155f4c7ca2af765f0626143b141fe3550
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-28 21:01:46 -0800

    Mostrando en el carrito el curso seleccionado

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -5,6 +5,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners(){
@@ -34,4 +35,39 @@ function leerDatosCurso(curso){
         cantidad: 1,
     }
     console.log(infoCurso);
+    
+    //agrega elementos al arreglo del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    console.table(articulosCarrito);
+    carritoHTML();
+}
+
+//Muestra el carrito de compras en el HTML
+function carritoHTML(){
+
+    //limpiar el html
+    limpiarHTML();
+
+
+    //recorre el carito y genera el html
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+function limpiarHTML(){
+    //Forma lenta
+    //contenedorCarrito.innerHTML = "";
+
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }
