5bc048de6a935061a80a0c614ac6d6a5c9e52688
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-29 19:55:24 -0800

    Validar los campos

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,12 +1,20 @@
 //Variables
 
 const btnEnviar = document.querySelector("#enviar");
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
 
 eventListeners()
 //eventListeners
 function eventListeners(){
+    //cuando la app arranca
     document.addEventListener("DOMContentLoaded", iniciarApp );
     btnEnviar.addEventListener("click", () => {});
+
+    email.addEventListener("blur", validarFormulario);
+    asunto.addEventListener("blur", validarFormulario);
+    mensaje.addEventListener("blur", validarFormulario);
 }
 
 
@@ -14,7 +22,22 @@ function eventListeners(){
 //funciones
 function iniciarApp(){
    // btnEnviar.disabled = true;
-    btnEnviar.classList.add("cursor-not-allowed opacity-50");
+    btnEnviar.classList.add("cursor-not-allowed","opacity-50");
+}
 
 
+//valida el formulario
+function validarFormulario(e){
+    
+    if(e.target.value.length > 0){
+        //console.log("si hay algo");
+        e.target.style.borderBottomColor = "";
+        e.target.classList.remove("border","border-red-500");
+        console.log("no hay algo");
+    }else{
+        //e.target.style.borderBottomColor = "red";
+        e.target.classList.add("border","border-red-500");
+        console.log("no hay algo");
+    }
+   
 }
