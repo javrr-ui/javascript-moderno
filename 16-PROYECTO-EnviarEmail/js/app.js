f5b8bbf700227a6e10865a580e7747739a399bf7
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-02-01 14:12:21 -0800

    Cambiar el color del input al pasar la validacion

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -32,22 +32,38 @@ function iniciarApp(){
 function validarFormulario(e){
     
     if(e.target.value.length > 0){
-        //console.log("si hay algo");
-        e.target.style.borderBottomColor = "";
+        
+        
+        //elimina los errores
+        const error = document.querySelector("p.error");
+        error.remove();
+        
         e.target.classList.remove("border","border-red-500");
+        e.target.classList.add("border","border-green-500");
         
         
     }else{
-        //e.target.style.borderBottomColor = "red";
+        e.target.classList.remove("border","border-green-500");
         e.target.classList.add("border","border-red-500");
         //console.log("no hay algo");
         mostrarError("Todos los campos son obligatorios");
     }
     console.log(e.target.type);
     if(e.target.type === "email"){
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
        
         const resultado = e.target.value.indexOf("@");
-        if(resultado < 0 ){
+
+        if(er.test(e.target.value)){
+            const error = document.querySelector("p.error");
+            error.remove();
+
+            e.target.classList.add("border","border-green-500");
+            console.log("email valido");
+        }else{
+            e.target.classList.remove("border","border-green-500");
+            e.target.classList.add("border","border-red-500");
+            console.log("email no valido")
             mostrarError("Email no válido");
         }
 
