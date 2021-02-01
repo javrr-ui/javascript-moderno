a67aa3183af498c0f71611740ebb020ed7f5ef1b
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-01-31 22:41:07 -0800

    Validar un email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -41,14 +41,26 @@ function validarFormulario(e){
         //e.target.style.borderBottomColor = "red";
         e.target.classList.add("border","border-red-500");
         //console.log("no hay algo");
-        mostrarError();
+        mostrarError("Todos los campos son obligatorios");
+    }
+    console.log(e.target.type);
+    if(e.target.type === "email"){
+       
+        const resultado = e.target.value.indexOf("@");
+        if(resultado < 0 ){
+            mostrarError("Email no válido");
+        }
+
     }
 }
 
-function mostrarError(){
+function mostrarError(mensaje){
     const mensajeError = document.createElement("p");
-    mensajeError.textContent = "Todos los campos son obligatorios";
+    mensajeError.textContent =mensaje;
     mensajeError.classList.add("border","border-red-500","background-red-100","text-red-500","p-3","mt-5","text-center", "error");
+    
+
+
 
     const errores = document.querySelectorAll(".error");
     if(errores.length ===0){
