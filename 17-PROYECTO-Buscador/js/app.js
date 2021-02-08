149355dfca35579025c098c8efac0dfe5632020b
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-02-07 19:31:13 -0800

    Filtrar autos por año

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -48,6 +48,7 @@ marca.addEventListener("change", e=>{
 
 year.addEventListener("change", e=>{
     datosBusqueda.year = e.target.value;
+    filtrarAuto();
 });
 
 minimo.addEventListener("change", e=>{
@@ -97,7 +98,7 @@ function llenarSelect(){
 
 //funcion que filtra en base a la busqueda
 function filtrarAuto(){
-    const resultado = autos.filter(filtrarMarca);
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
 
     console.log(resultado);
 }
@@ -108,5 +109,12 @@ function filtrarMarca(auto){
        return auto.marca === marca;
     }
     return auto;
-    
+}
+
+function filtrarYear(auto){
+    const {year} = datosBusqueda;
+    if(year){
+        return auto.year === parseInt(year);
+    }
+    return auto;
 }
