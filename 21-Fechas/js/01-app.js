2a53120ba195f0064e546bb2cf467367dc691da0
Javier <framirez76@uabc.edu.mx>
2021-09-29 20:51:23 -0700

    Comment code

--- 21-Fechas/js/01-app.js
+++ 21-Fechas/js/01-app.js
@@ -1,26 +1,26 @@
-const diaHoy = new Date();
-const algunDia = new Date("3-10-1999");
-let valor;
+// const diaHoy = new Date();
+// const algunDia = new Date("3-10-1999");
+// let valor;
 
-valor = diaHoy;
-console.log(valor);
-console.log(algunDia);
+// valor = diaHoy;
+// console.log(valor);
+// console.log(algunDia);
 
-// getFullYear
+// // getFullYear
 
-valor = diaHoy.getFullYear();
-console.log("getFullYear(): "+valor);
+// valor = diaHoy.getFullYear();
+// console.log("getFullYear(): "+valor);
 
-// get month
+// // get month
 
-valor = diaHoy.getMonth();
-// january = 1, february = 2, etc
-console.log(valor);
+// valor = diaHoy.getMonth();
+// // january = 1, february = 2, etc
+// console.log(valor);
 
-// get hours
-valor = diaHoy.getHours();
-console.log(valor);
+// // get hours
+// valor = diaHoy.getHours();
+// console.log(valor);
 
-//get minutes
-valor = diaHoy.getMinutes();
-console.log(valor);
+// //get minutes
+// valor = diaHoy.getMinutes();
+// console.log(valor);
