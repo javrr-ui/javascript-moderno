f9b95880e6704b8317a6cba871f733dd4225562c
Javier <framirez76@uabc.edu.mx>
2021-09-29 20:34:57 -0700

    getFullYear()

--- 21-Fechas/js/01-app.js
+++ 21-Fechas/js/01-app.js
@@ -1,5 +1,12 @@
 const diaHoy = new Date();
+const algunDia = new Date("3-10-1999");
 let valor;
 
 valor = diaHoy;
 console.log(valor);
+console.log(algunDia);
+
+// getFullYear
+
+valor = diaHoy.getFullYear();
+console.log("getFullYear(): "+valor);
