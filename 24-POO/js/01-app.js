df99d82f844b8f7d69844c6bb9b8b5c140e61793
Javier <framirez76@uabc.edu.mx>
2021-10-04 21:32:26 -0700

    Métodos y métodos estaticos en las clases

--- 24-POO/js/01-app.js
+++ 24-POO/js/01-app.js
@@ -4,19 +4,33 @@ class Cliente {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`;
+    }
+
+    static bienvenida() {
+        return "Bienvenido";
+    }
 }
 
 const javier = new Cliente("Javier", 5000);
 console.log(javier);
-
+console.log(javier.mostrarInformacion());
+console.log(Cliente.bienvenida());
 // Class expression
 const Cliente2 = class {
     constructor(nombre, saldo) {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`;
+    }
 }
 
 
 const javier2 = new Cliente("Javier", 5000);
 console.log(javier2);
+console.log(javier2.mostrarInformacion());
